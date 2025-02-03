3a8c48d195a396a1ba67b2235129bd8f63edde08
futuredeveloper41 <mj10283@naver.com>
2025-02-03 09:39:55 +0900

    # JSX 기초 문법

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,71 @@
+import React from 'react';
 
+// 컴포넌트
 function App() {
 
+  /*
+    jsx : 리액트에서 사용하는 특수한 js 문법, 태그를 그대로 쓰면 알아서 변환
+   */
+
+  // 원래 문법 :
+  // const $h1 = React.createElement('h1', null, 'React 101');
+
+  // 축약 문법 :
+  const $h1 = <h1>React 101</h1>;
+  // return $h1;
+
+  // 규칙 1. return안에있는 태그는 반드시 하나의 태그로 묶여야 함.
+  // return <h1>ㅎㅎ</h1><h2>ㅍㅍ</h2>;
+  // return (
+  //     <header>
+  //       <h1>React</h1>
+  //       <h2>102</h2>
+  //     </header>
+  // );
+
+  // 규칙 2. 빈 태그(닫는 태그가 없는)는 반드시 /> 로 마감
+  // return (
+  //     <header>
+  //       <input type='text'/>ㅎㅎ
+  //     </header>
+  // );
+
+  // 규칙 3. 태그의 class속성은 자바스크립트 키워드 class와 겹쳐서 className으로 표기
+  //        label for 은 label htmlfor로
+  // return (
+  //     <header className='ㅎㅎ'>
+  //       <h1>React</h1>
+  //       <label htmlFor='username'></label>
+  //       <input type='text' id='username' />
+  //     </header>
+  // );
+
+ // 규칙 4. 의미없는 부모는 <React.Fragment>로 감싸면 됨 (혹은 <>
+  const subject =  <span>ㅎㅎㅎ</span>
   return (
-    <h1>리액트 레스트</h1>
-  )
+      <>
+        <header className='ㅎㅎ'>
+          <h1>React {subject}</h1>
+          <label htmlFor='username'> </label>
+          <input type='text' id='username' />
+        </header>
+        <main> 메인 컨텐츠~~</main>
+      </>
+  );
+
+  // 5. 변수값이나 함수를 출력할 때는 {}로 감싸면 됨.
+  // const subject =  <span>ㅎㅎㅎ</span>
+  //  return (
+  //     <header className='ㅎㅎ'>
+  //       <h1>React {subject}</h1>
+  //       <label htmlFor='username'> </label>
+  //       <input type='text' id='username' />
+  //     </header>
+  // );
+
 }
 
 export default App
+
 
 
