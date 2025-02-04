68e49228504c4f6b0a18fc7cc8650c20afb028f8
futuredeveloper41 <mj10283@naver.com>
2025-02-04 11:56:38 +0900

    # 객체로 상태관리(expenseform.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,6 @@ const App = () => {
 
             // 안의 태그 내용이 유동적인 경우 : 부모가 <></> 사이에 내용 적어서 children으로 전달해주면, 자식 태그에서 props.children으로 사용
             <>
-                <CheckBoxStyle />
                 <NewExpense />
                 <ExpenseList expenses={expenses} />
             </>
--- src/components/new-expense/ExpenseForm.jsx
+++ src/components/new-expense/ExpenseForm.jsx
@@ -14,21 +14,63 @@ const ExpenseForm = () => {
 
     // 1. 상태값 관리하기
     //   <input type="text" onChange={e => setTitle(e.target.value)}/>
-    const [title, setTitle] = useState('');
-    const [price, setPrice] = useState(0);
-    const [date, setDate] = useState(null);
+
+    // 하나씩
+    // const [title, setTitle] = useState('');
+    // const [price, setPrice] = useState(0);
+    // const [date, setDate] = useState(null);
+
+    const initialUserInput = {
+        title: '',
+        price: 0,
+        date: '',
+    };
+
+    // 여러 개 한 번에
+    const [userInput, setUserInput] = useState(initialUserInput);
+
 
     // 3. form에 걸 이벤트 만들기
     const handleSubmit = e => {
         e.preventDefault();
 
-        const payload = {
-            title,
-            price,
-            date
-        };
+        // 단일 값 할 때
+        // const payload = {
+        //     title,
+        //     price,
+        //     date
+        // };
 
-        console.log('payload : ', payload);
+        console.log('payload : ', userInput);
+
+        // 입력창 비우기 - 상태값과 input을 연결하려면 양방향으로 연결해야 함
+        // input 태그에 입력하면 상태값 userInput만 변경됨 - 단방향
+        // setUserInput(null); !! <- 이렇게 하면 지워지지도 않고 순서 때문에 payload도 null이 되어 버림 !!!!
+        setUserInput(initialUserInput);
+
+
+    }
+
+    // 객체로 상태관리하깅 위해서, 쉽게 하기 위해 함수 빼줌
+    // ! 상태값이 객체나 배열일 경우에는 항상 새로운 객체, 배열을 통해 세팅하여야 함
+    const handleTitleInput = (e) => {
+        setUserInput({
+            // 객체 복사 후 기존에 있는 key 값을 다시 설정하면, 그 key의 값이 수정됨 !
+            ... userInput,
+            title: e.target.value,
+        })
+    }
+    const handlePriceInput = (e) => {
+        setUserInput({
+            ... userInput,
+            price: e.target.value
+        })
+    }
+    const handleDateInput = (e) => {
+        setUserInput({
+            ...userInput,
+            date: e.target.value
+        })
     }
 
     return (
@@ -37,7 +79,12 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={e => setTitle(e.target.value)}/>
+                    {/* 단일 상태 관리 값으로 관리할 떄 : onChange={e => setTitle(e.target.value)} */}
+                    <input
+                        type="text"
+                        onChange={ handleTitleInput }
+                        // ???? 이래야 value 값이 바뀐다고??
+                        value={userInput.title} />
                 </div>
                 <div className="new-expense__control">
                     <label>Price</label>
@@ -45,7 +92,8 @@ const ExpenseForm = () => {
                         type="number"
                         min="100"
                         step="100"
-                        onChange={e => setPrice(+e.target.value)}
+                        onChange={ handlePriceInput }
+                        value={userInput.price}
                     />
                 </div>
                 <div className="new-expense__control">
@@ -54,7 +102,8 @@ const ExpenseForm = () => {
                         type="date"
                         min="2019-01-01"
                         max={getTodayDate()}
-                        onChange={e => setDate(e.target.value)}
+                        onChange={ handleDateInput }
+                        value={userInput.date}
                     />
                 </div>
             </div>
