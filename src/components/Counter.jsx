5fcac10748528c8c6087eeb7195ce54d7e684fa3
futuredeveloper41 <mj10283@naver.com>
2025-02-04 10:33:37 +0900

    # Counter.jsx로 상태관리 복습

---
+++ src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+const Counter = () => {
+
+    //  상태 관리 하려면 일반 변수가 아니라 useState로 관리해야 함
+    // useState의 리턴값 : 1. 상태관리 변수값 2. 상태변경 함수
+    const [count, setCount] = useState(0);
+    console.log('1. init count: ', count);
+
+    const increaseHandler = () => {
+        // 상태 값을 업데이트할 때는, 반드시  setter를 통해 업데이트해야 함
+        // count++; 이렇게 하면 안 됨
+        setCount(count + 1);
+        // 상태 업데이트는 리렌더링 이후에 일어남
+        console.log('2. click count: ', count);
+    };
+
+    const decreaseHandler = () => setCount(count - 1);
+
+    console.log('3. last count: ', count);
+    return (
+        <div>
+            <h1>숫자: {count}</h1>
+            <button onClick={increaseHandler}>증가</button>
+            <button onClick={decreaseHandler}>감소</button>
+
+        </div>
+    );
+};
+
+export default Counter;
