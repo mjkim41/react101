3e3671c1f4aaf11aa5b0ccf28465f35102292405
futuredeveloper41 <mj10283@naver.com>
2025-02-05 10:38:25 +0900

    # NewExpense.jsx : 새로운 지출 추가하는 버튼 만들기

--- src/components/new-expense/NewExpense.jsx
+++ src/components/new-expense/NewExpense.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-// drilling
 const NewExpense = ({ onSave }) => {
+
+
+    // 새로운 지출 버튼을 눌렀는지 여부를 상태관리
+    const [toggle, setToggle] = useState(false);
+
+    const buttonComponent = <button onClick={() => setToggle(true)}>새로운 지출 추가하기</button>;
+    const formComponent = <ExpenseForm onAdd={onSave} />;
+
     return (
-        <div className="new-expense">
-            <ExpenseForm onAdd ={ onSave }/>
+        <div className='new-expense'>
+            {toggle ? formComponent : buttonComponent}
         </div>
     );
 };
