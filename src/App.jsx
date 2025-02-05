56960fe5186c792aec613c14b87a4263073b7773
futuredeveloper41 <mj10283@naver.com>
2025-02-05 12:35:12 +0900

    # Course 폴더 전체 흐름

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import './App.css';
 import CourseInput from "./components/CourseGoals/CourseInput.jsx";
 import CourseList from "./components/CourseGoals/CourseList.jsx";
+import {useState} from "react";
 
 // 컴포넌트
 const App = () => {
 
+    // 목표 쌓아야 되니까 배열로 관리
+    const [goals, setGoals] = useState([]);
+
+    // 바구니 함수(CourseInput에 전달할 함수)
+    const onAddGoal = ( newGoal ) => {
+        setGoals( [ ...goals, newGoal ] );
+    }
+
     return (
         <div>
             <section id={'goal-form'}>
-                <CourseInput/>
+                <CourseInput onAdd={ onAddGoal }/>
             </section>
             <section id='goals'>
-                <CourseList />
+                <CourseList items={goals}/>
             </section>
         </div>
     );
--- src/components/CourseGoals/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './CourseInput.css';
 import Button from "../ui/Button.jsx";
 
-const CourseInput = () => {
+// onAdd : App에 goal 전달용 임의 함수
+const CourseInput = ({onAdd}) => {
+
+
+
+    const [enteredText, setEnteredText] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(enteredText);
+        // 부모에 goal 보내주기
+        onAdd(enteredText);
+
+        // 전송 끝나면 입력창 비우기
+        setEnteredText('');
+        document.querySelector('input').focus();
+    };
+
+    const handleGoalInput = e => {
+        const inputValue = e.target.value;
+        setEnteredText(inputValue);
+
+    }
+
     return (
-        <form>
+        <form onSubmit={ handleSubmit }>
             <div className='form-control'>
                 <label>나의 목표</label>
-                <input type='text' />
+                <input type='text' onInput={ handleGoalInput } value={ enteredText }/>
             </div>
             <Button type='submit'>목표 추가하기</Button>
         </form>
