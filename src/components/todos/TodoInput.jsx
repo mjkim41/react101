634837f631930d3064fd4137adcdeb27cc280171
mjkim41 <mj10283@naver.com>
2025-02-08 08:47:45 +0900

    # Todoinput.jsx : 버튼 클릭 여부에 따라 입력창 열고 닫기

--- src/components/todos/TodoInput.jsx
+++ src/components/todos/TodoInput.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { MdAdd } from 'react-icons/md';
 
 import styles from './scss/TodoInput.module.scss';
 
 const TodoInput = () => {
+
+    console.log(styles);
+
+    const { 'form-wrapper':formWrapper, 'insert-btn':insertBtn, 'insert-form':insertForm, open:openStyle} = styles;
+
+    // [버튼 클릭 시 입력창 열리는 이벤트]
+    //   2. 입력창 여는 여부를 useState로 관리
+    const [open, setOpen] = useState(false);
+
     return (
         <>
-            <div className='form-wrapper'>
-                <form className='insert-form'>
-                    <input
-                        type='text'
-                        placeholder='할 일을 입력 후, 엔터를 누르세요!'
-                    />
-                </form>
-            </div>
-            <button className='insert-btn'>
-                <MdAdd />
+            {
+                /* [버튼 클릭 시 입력창 열리는 이벤트]
+                       3. 입력창 여는 여부 확인하는 변수의 값에 따라, 태그 표시 or 미표시 */
+                open &&
+                    <div className={insertBtn}>
+                        <form className={insertForm}>
+                            <input
+                                type='text'
+                                placeholder='할 일을 입력 후, 엔터를 누르세요!'
+                            />
+                        </form>
+                    </div>
+
+            }
+
+            {/* [버튼 클릭 시 입력창 열리는 이벤트]
+               1. 클릭이벤트 걸어주기 */}
+            <button className={ `${insertBtn} ${open && openStyle}` } onClick={() => setOpen(!open)}>
+                <MdAdd/>
             </button>
         </>
     );
