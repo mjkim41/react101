a9776f38d9f28dd0bb74520d4bf374961e2479d1
mjkim41 <mj10283@naver.com>
2025-02-08 14:38:15 +0900

    # AddUsers : useRef()를 사용하여 직접 dom의 속성 변경

--- src/components/Users/AddUsers.jsx
+++ src/components/Users/AddUsers.jsx
@@ -1,35 +1,50 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import styles from './AddUsers.module.css';
 import Card from '../ui/Card';
 import Button from '../ui/Button';
 import ErrorModal from "../ui/modal/ErrorModal.jsx";
 const AddUsers = ({onAddUser}) => {
 
-    const [userValue, setUserValue] = useState({
-        username: '',
-        age: ''
-    })
+    // useRef로 태그 기억하기
+    const usernameRef = useRef();
+    const ageRef = useRef();
+
+    // console.log(usernameRef);
+
+    // const [userValue, setUserValue] = useState({
+    //     username: '',
+    //     age: ''
+    // })
 
     // 에러가 났을 때 에러의 데이터를 관리할 상태변수
     // error -> {title: '에러 제목', message: '에러 내용' }
     const [error, setError] = useState(null);
 
-    const handleName = (e) => {
-        setUserValue({
-            ...userValue,
-            username: e.target.value
-        });
-    }
-
-    const handleAge = (e) => {
-        setUserValue({
-            ...userValue,
-            age: e.target.value
-        });
-    }
+    // const handleName = (e) => {
+    //     setUserValue({
+    //         ...userValue,
+    //         username: e.target.value
+    //     });
+    // }
+    //
+    // const handleAge = (e) => {
+    //     setUserValue({
+    //         ...userValue,
+    //         age: e.target.value
+    //     });
+    // }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        // ###############
+        const $username = usernameRef.current;
+        const $age = ageRef.current;
+
+        const userValue = {
+            username: $username.value,
+            age: $age.value
+        }
 
         // 입력값 검증
         if (!userValue.username.trim()) {
@@ -52,10 +67,13 @@ const AddUsers = ({onAddUser}) => {
             id: Math.random().toString()
         })
 
-        setUserValue({
-            username: '',
-            age: ''
-        });
+        $username.value = '';
+        $age.value = '';
+
+        // setUserValue({
+        //     username: '',
+        //     age: ''
+        // });
     }
 
     // 모달을 닫아주는 함수
@@ -74,15 +92,17 @@ const AddUsers = ({onAddUser}) => {
                     <input
                         id='username'
                         type='text'
-                        value={userValue.username}
-                        onInput={handleName}
+                        // value={userValue.username}
+                        // onInput={handleName}
+                        ref={usernameRef}
                     />
                     <label htmlFor='age'>나이</label>
                     <input
                         id='age'
                         type='number'
-                        value={userValue.age}
-                        onInput={handleAge}
+                        // value={userValue.age}
+                        // onInput={handleAge}
+                        ref={ageRef}
                     />
                     <Button type='submit'>가입하기</Button>
                 </form>
