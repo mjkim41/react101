9e00f143ed0b79de6fbb2773f90444a832149c6c
mjkim41 <mj10283@naver.com>
2025-02-08 15:45:49 +0900

    # Login : 기본 검증

--- src/components/SideEffect/Login.jsx
+++ src/components/SideEffect/Login.jsx
@@ -4,29 +4,74 @@ import styles from './Login.module.css';
 import Button from '../ui/Button';
 const Login = () => {
 
+    // 사용자가 입력한 이메일, 패스워드를 상태 관리
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+
+    // 이메일, 패스워드를 잘 썻는지 확인하는 변수
+    const [emailIsValid, setEmailIsValid] = useState(null);
+    const [passwordIsValid, setPasswordIsValid] = useState(null);
+
+    // 로그인 버튼을 열어줄지를 관리하는 변수
+    const [formIsValid, setFormIsValid] = useState(false);
+
+    // 이메일 값 저장 및 검증
+    const handleEmail = e => {
+        setEnteredEmail(e.target.value);
+
+        setFormIsValid(
+            e.target.value.includes('@') && enteredPassword.trim().length>6
+        );
+    };
+
+    // 패스워드 값 저장 및 검증
+    const handlePassword = e => {
+        setEnteredPassword(e.target.value);
+
+        setFormIsValid(enteredEmail.includes('@') && e.target.value.trim().length > 6);
+    };
+
+    // 이메일 검증
+    const validateEmail = e => {
+        setEmailIsValid(enteredEmail.includes('@'));
+    }
+
+    // 패스워드 검증
+    const validatePassword = e => {
+        setPasswordIsValid(enteredPassword.trim().length > 6 );
+    }
+
+
     return (
         <Card className={styles.login}>
             <form>
                 <div
-                    className={`${styles.control}`}>
+                    className={`${styles.control} ${emailIsValid === false ? styles.invalid : ''}`}>
                     <label htmlFor='email'>E-Mail</label>
                     <input
                         type='email'
                         id='email'
+                        value={enteredEmail}
+                        onInput={handleEmail}
+                        onBlur={validateEmail}
                     />
                 </div>
                 <div
-                    className={`${styles.control}`}>
+                    className={`${styles.control} ${passwordIsValid === false ? styles.invalid : ''} `}>
                     <label htmlFor='password'>Password</label>
                     <input
                         type='password'
                         id='password'
+                        value={enteredPassword}
+                        onInput={handlePassword}
+                        onBlur={validatePassword}
                     />
                 </div>
                 <div className={styles.actions}>
                     <Button
                         type='submit'
                         className={styles.btn}
+                        disabled={!formIsValid}
                     >
                         Login
                     </Button>
