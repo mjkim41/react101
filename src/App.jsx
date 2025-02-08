87451806695630b55b4705437875c23985a06f17
mjkim41 <mj10283@naver.com>
2025-02-08 11:20:30 +0900

    # User 관리 플랫폼 : 추가 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 
 import { useState } from 'react';
 import './App.css';
-import TodoTemplate from './components/Todos/TodoTemplate';
+import AddUsers from "./components/Users/AddUsers.jsx";
+import UserList from "./components/Users/UserrList.jsx";
 
 // 컴포넌트
 const App = () => {
-    return <TodoTemplate />;
+
+    // 회원 저장 배열
+    const [userList, setUserList] = useState([]);
+
+    // 입력한 회원정보를 가져오는 함수
+    const addUserInfo = (user) => {
+        setUserList([...userList, user]);
+    }
+    return <>
+        <AddUsers onAddUser={addUserInfo}/>
+        <UserList users={userList}/>
+    </>;
 };
 
 export default App;
