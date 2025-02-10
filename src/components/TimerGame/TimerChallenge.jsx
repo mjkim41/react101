90865e5b8935a6dfd06e620e8987c66b0b0280ee
futuredeveloper41 <mj10283@naver.com>
2025-02-10 10:47:34 +0900

    # Countdown : useRef로 component 값을 재렌더링해도 기억하게 하기 (TimerChallenge.jsx)

--- src/components/TimerGame/TimerChallenge.jsx
+++ src/components/TimerGame/TimerChallenge.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 const TimerChallenge = ({ title, targetTime }) => {
+
+    // 타이머 id를 컴포넌트별로 각각 관리 - 리렌더링이 되어도 값이 유지
+    const timer = useRef();
 
     // 타이머가 시작되었는지 확인하는 상태값
     const [timerStarted, setTimerStarted] = useState(false);
 
     // 시간이 다 지났는지 여부
     const [timerExpired, setTimerExpired] = useState(false);
-
-    // 타이머 id 저장
-    let timer;
 
     const handleStart = () => {
 
@@ -23,19 +23,19 @@ const TimerChallenge = ({ title, targetTime }) => {
          */
 
         // 실제 시간을 실행
-        timer = setTimeout(() => {
+        timer.current = setTimeout(() => {
             console.log(`${targetTime} 초가 지남!`);
             setTimerExpired(true);
         }, targetTime * 1000);
 
-        console.log(`start timer: ${timer}`);
+        console.log(`start timer: ${timer.current}`);
 
     }
 
     // stop 이벤트
     const handleStop = () => {
-        console.log('타이머 중지 stop timer: ', timer);
-        clearTimeout(timer);
+        console.log('타이머 중지 stop timer: ', timer.current);
+        clearTimeout(timer.current);
     }
 
     return (
