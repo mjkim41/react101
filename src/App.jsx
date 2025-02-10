bf073ddc68b7240ea64220218b1e2fb3c6b3e0f6
futuredeveloper41 <mj10283@naver.com>
2025-02-10 10:26:48 +0900

    # Countdown : TimercHALLENGE.JSX의 타이머 시작 이벤트

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Player from "./components/TimerGame/Player.jsx";
+import TimerChallenge from "./components/TimerGame/TimerChallenge.jsx";
 
 
 // 컴포넌트
@@ -8,6 +9,12 @@ const App = () => {
 
     return <>
         <Player />
+        <div id={'challenges'}>
+            <TimerChallenge title='Easy' targetTime={1} />
+            <TimerChallenge title='Not Easy' targetTime={5} />
+            <TimerChallenge title='Getting tough' targetTime={10} />
+            <TimerChallenge title='Pros only' targetTime={15} />
+        </div>
     </>;
 };
 
