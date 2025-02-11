3db15fe9a5c874251306a60caac3bb4dd73bb942
futuredeveloper41 <mj10283@naver.com>
2025-02-11 09:51:24 +0900

    # Player.jsx : forwardRef - 주석 추가

--- src/components/TimerGame/Player.jsx
+++ src/components/TimerGame/Player.jsx
@@ -20,6 +20,7 @@ const Player = () => {
         <section id="player">
             <h2>Welcome { enteredName }</h2>
             <p>
+                {/* DOM에 접근하기 위해서 Input에 ref 를 달음 -> 근데 ref는 HTML 태그에만 되는데? 그럼 props으로보내면 안 되나? 응 안돼. 그럼 어떻게? forwardRef를 이용해서 */}
                 <Input type="text" ref={nameRef}/>
                 <button onClick={handleChangeName}>Set Name</button>
             </p>
